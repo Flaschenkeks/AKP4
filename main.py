68e707315ff855bdd29ec5557c2d8af5a83c0a11
Desktopbrot <Lars.Niederweis@gmx.de>
2023-05-29 16:12:22 +0200

    Retter in der Not Jonathan saves the day: A1 works

--- main.py
+++ main.py
@@ -1,29 +1,9 @@
-import base64
-import hashlib
-from Crypto import Random
 from Crypto.Cipher import AES
+import binascii
 
-class AESCipher(object):
+key = binascii.unhexlify('000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f')
 
-    def __init__(self, key):
-        self.bs = AES.block_size
-        self.key = hashlib.sha256(key.encode()).digest()
+cipher = AES.new(key, AES.MODE_ECB)
+msg = cipher.encrypt(binascii.unhexlify('00112233445566778899aabbccddeeff'))
 
-    def encrypt(self, raw):
-        raw = self._pad(raw)
-        iv = Random.new().read(AES.block_size)
-        cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        return base64.b64encode(iv + cipher.encrypt(raw.encode()))
-
-    def decrypt(self, enc):
-        enc = base64.b64decode(enc)
-        iv = enc[:AES.block_size]
-        cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
-
-    def _pad(self, s):
-        return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
-
-    @staticmethod
-    def _unpad(s):
-        return s[:-ord(s[len(s)-1:])]
+print(binascii.hexlify(msg))
